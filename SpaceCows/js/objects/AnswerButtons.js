614f21683b336073e9b8485080b38b4c86772024
Keyyva <keyyva@gmail.com>
2018-11-04 00:31:24 -0400

    Questions and answers working properly!

--- SpaceCows/js/objects/AnswerButtons.js
+++ SpaceCows/js/objects/AnswerButtons.js
@@ -11,7 +11,7 @@
 
 class AnswerButtons {
 	// AnswerButtons private variables
-	constructor(){		
+	constructor(){
 		// Buttons
 		this.button1;
 		this.button2;
@@ -26,10 +26,17 @@ class AnswerButtons {
 		this.answer2;
 		this.answer3;
 		
+		// Questions and Answers
+		this.generator;
+		this.QA = {q1: 0, a: 0, q2: 0};		// Questions are formatted as; q1 X a = q2
+		
 		this.create();
 	}
 	
 	create(){
+		// Questions and Answers
+		this.generator = new QuestionAnswerGenerator();
+		
 		// Buttons
 		this.button1 = game.add.button(spaceCowsBackground.x - spaceCowsBackground.width/3, spaceCowsBackground.y + spaceCowsBackground.height / 3, "ButtonAnswer", this.button1Click, this, this.buttonType.HOVER, this.buttonType.IDLE, this.buttonType.CLICK);
 		this.button1.anchor.setTo(0.5);
@@ -39,36 +46,84 @@ class AnswerButtons {
 		this.button3.anchor.setTo(0.5);
 		
 		// Text
+		this.question = game.add.text(spaceCowsBackground.x, spaceCowsBackground.y, this.getQuestion(), {font: "70px Arial", fill: "#000"});
 		this.answer1 = game.add.text(this.button1.x, this.button1.y, "0", {font: "70px Arial", fill: "#000"});
 		this.answer1.anchor.setTo(0.5);
 		this.answer2 = game.add.text(this.button2.x, this.button3.y, "1", {font: "70px Arial", fill: "#00"});
 		this.answer2.anchor.setTo(0.5);
-		this.answer3 = game.add.text(this.button3.x, this.button3.y, "5", {font: "70px Arial", fill: "#000"});
+		this.answer3 = game.add.text(this.button3.x, this.button3.y, "3", {font: "70px Arial", fill: "#000"});
 		this.answer3.anchor.setTo(0.5);
 		
 		
 		this.reset();
 	}
 	
-	// Resets the question and answer (also creates the first question and answer)
+	/*** Resets the question and answer (also creates the first question and answer) ***/
 	reset(){
+		this.QA = this.generator.generate();
 		
+		// Set the question
+		this.question.text = this.getQuestion();
+		
+		// Fill last answer button with a random if it's not the answer
+		if(this.QA.a == 0 || this.QA.a == 1){
+			this.answer3.text = Math.floor((Math.random() * 6) + 2);
+		}
+		// Fill the last answer button with the answer
+		else{
+			this.answer3.text = this.QA.a;
+		}
+	}
+	
+	/*** A continue button is displayed, when clicked the game get reset ***/
+	// When the user picks the correct answer, this function is called
+	correct(){
+		// TODO: Display "continue" button. When clicked, reset the game.
+	}
+	
+	/*** Returns the question as a string ***/
+	getQuestion(){
+		return String(this.QA.q1) + " x ? = " + String(this.QA.q2);
 	}
 	
 	
 	/********** BUTTON FUNCTIONS **********/
 	/*** Called when button1 is clicked ***/
 	button1Click(){
-		// TODO: test if answer1 is the answer
+		if(parseInt(this.answer1.text) == this.QA.a){
+			// CORRECT
+			console.log("Correct!");
+			this.correct();
+		}
+		else{
+			// INCORRECT
+			console.log("Try Again");
+		}
 	}
 	
 	/*** Called when button2 is clicked ***/
 	button2Click(){
-		// TODO: test if answer2 is the answer
+		if(parseInt(this.answer2.text) == this.QA.a){
+			// CORRECT
+			console.log("Correct!");
+			this.correct();
+		}
+		else{
+			// INCORRECT
+			console.log("Try Again");
+		}
 	}
 	
 	/*** Called when button3 is clicked ***/
 	button3Click(){
-		// TODO: test if answer3 is the answer
+		if(parseInt(this.answer3.text) == this.QA.a){
+			// CORRECT
+			console.log("Correct!");
+			this.correct();
+		}
+		else{
+			// INCORRECT
+			console.log("Try Again ");
+		}
 	}
 }
--- SpaceCows/js/objects/QuestionAnswerGenerator.js
+++ SpaceCows/js/objects/QuestionAnswerGenerator.js
@@ -10,46 +10,42 @@
 
 class QuestionAnswerGenerator {
 	constructor(){
-		this.question = {q1: 0, q2: 0};		// First and second parts of given question
-		this.a;								// The answer to the question
+		this.QA = {q1: 0, a: 0, q2: 0};		// Questions are formatted as; q1 X a = q2
 	}
 	
 	/*** Generates a question and stores the answer. Returns the two parts of the question as an enum ***/
 	generate(){
 		// Questions go up to 7x7. Possible answers are; 0, 1 or another number.
-		var rand = Math.floor((Math.random() * 4)); 	// Random number to choose which type of question.
+		var rand = Math.floor((Math.random() * 3)); 	// Random number to choose which type of question.
 		console.log("The type of question is: " + rand);
 		
 		switch(rand){
 		// Sets q1=random	q2=0	a=0
 		case 0:
-			this.question = {q1: Math.floor((Math.random() * 8)), q2: 0}
-			this.a = 0;
+			this.QA = {q1: Math.floor((Math.random() * 8)), a: 0, q2: 0}
+			console.log(this.QA.q1 + " q0 " + this.QA.q2);
 			break;
 			
 		// Sets q1=random	q2=same random	a=1
 		case 1:
-			var rand = Math.floor((Math.random() * 7) + 1);
-			this.question = {q1: rand, q2: rand}
-			this.a = 1;
+			rand = Math.floor((Math.random() * 7) + 1);
+			console.log(rand);
+			this.QA = {q1: rand, a: 1, q2: rand}
+			console.log(this.QA.q1 + " q1 " + this.QA.q2);
 			break;
 			
 		// Sets q1=random	a=random	q2=q1*a
-		case 3:
-			this.question.q1 = Math.floor((Math.random() * 6) + 2);
-			this.a = Math.floor((Math.random() * 6) + 2);
-			this.question.q2 = this.question.q1 * this.a;
+		case 2:
+			this.QA.q1 = Math.floor((Math.random() * 6) + 2);
+			this.QA.a = Math.floor((Math.random() * 6) + 2);
+			this.QA.q2 = this.QA.q1 * this.QA.a;
+			console.log(this.QA.q1 + " q2 " + this.QA.q2);
 			break;
 			
 		default:
 				console.error("Error in QuestionAnswerGenerator.js, generate(). Random number out of bounds!");
 		}
 		
-		return this.question;
-	}
-	
-	
-	/*** Compares "check" to the answer for the question. Returns a bool ***/
-	checkAnswer(check){
+		return this.QA;
 	}
 }
